5a9cb43dabb8100bb0333c7743fa3b74cc0e519d
TQP0403 <fantasy.tqp@gmail.com>
2024-02-02 11:37:07 +0700

    [update] code

--- src/helper/slice.helper.go
+++ src/helper/slice.helper.go
@@ -1,7 +1,5 @@
 package helper
 
 func SliceCut(index int, slice []any) (any, []any) {
-	res := slice[index]
-	nSli := append(slice[:index], slice[index+1:]...)
-	return res, nSli
+	return slice[index], append(slice[:index], slice[index+1:]...)
 }
